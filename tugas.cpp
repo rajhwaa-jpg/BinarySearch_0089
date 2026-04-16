b052716875740b04e26a694404898af93196c71b
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-16 17:15:53 +0700

    add input prompts for array elements after size validation

--- tugas.cpp
+++ tugas.cpp
@@ -16,4 +16,14 @@ void input() {
             cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10, Silahkan coba lagi.\n";
         }
     }
+
+        cout << "\n=============================\n";
+    cout << "      Masukkan element Array     \n";
+    cout << "===============================\n";
+
+    for (int i = 0; i < nPanjang; i++) {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
 }
+
