0ef7e86136c5f93fb00d5a76fa67a0695805e310
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-16 17:20:25 +0700

    add display logic to show sorted array elements

--- tugas.cpp
+++ tugas.cpp
@@ -46,4 +46,12 @@ void display() {
     cout << "\n===========================\n";
     cout << " Element Array Setelah Diurutkan (Asc)\n"; 
     cout << "==============================\n";
+
+      for (int j = 0; j < nPanjang; j++) {
+        cout << element[j];
+        if (j < nPanjang - 1) {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
 }
