ac6782ef819f15fa6f3cfdb3c1a115fbbccd135f
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-16 17:23:31 +0700

    fix: complete binary search implementation with missing logic

--- tugas.cpp
+++ tugas.cpp
@@ -72,6 +72,20 @@ void binarySearch()
         int high = nPanjang - 1;
 
         do
+
+           {
+            int mid = (low + high) / 2;
+
+            if (element[mid] == x)
+            {
+                cout << "\n[√] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
     }
-}
+ }
         
