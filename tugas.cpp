230a351f2d497330f8b5712133bafc6fe8cf520e
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-16 17:22:25 +0700

    add binary search function for element lookup

--- tugas.cpp
+++ tugas.cpp
@@ -55,3 +55,23 @@ void display() {
     }
     cout << endl;
 }
+
+void binarySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n============================================\n";
+        cout << "         Pencarian Binary Search           \n";
+        cout << "============================================\n";
+
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+    }
+}
+        
