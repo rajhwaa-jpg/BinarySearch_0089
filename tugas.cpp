eff71765a9af53bbc8e07c61c7e5d4b63c9178eb
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-16 17:15:10 +0700

    implement input function to validate array size with user prompts

--- tugas.cpp
+++ tugas.cpp
@@ -4,3 +4,16 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input() {
+    while (true) {
+        cout << "Masukkan banyaknya element pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10) {
+            break;
+        } else {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10, Silahkan coba lagi.\n";
+        }
+    }
+}
